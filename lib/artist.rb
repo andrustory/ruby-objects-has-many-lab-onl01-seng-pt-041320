ad3c9a7150bf4574d65f180a68f759757e015538
Andres Carvajal <jc1993box@hotmail.com>
2020-05-13 02:43:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,13 +26,3 @@ def self.song_count
 end 
 end
 
-
-
-
-# has many songs (FAILED - 1)
-#   #add_song
-#     takes in an argument of a song and associates that song with the artist by telling the song that it belongs to that artist (FAILED - 2)
-#   #add_song_by_name
-#     takes in an argument of a song name, creates a new song with it and associates the song and artist (FAILED - 3)
-#   .song_count
-#     is a class method that returns the total number of songs associated to all existing artists (FAILED - 4)
