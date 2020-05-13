382b5b20ffcc204543341e93f65e57fcdeb8d7f8
Andres Carvajal <jc1993box@hotmail.com>
2020-05-13 02:29:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,12 +23,3 @@ end
 
 end
 
-
-# it 'knows the name of its artist' do
-#       drake = Artist.new('Drake')
-#       song.artist = drake
-#       expect(song.artist_name).to eq('Drake')
-#     end
-
-#     it 'returns nil if the song does not have an artist' do
-#       expect(song.artist_name).to eq nil
