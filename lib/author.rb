25881d1688f268d02680f9164642e67f3e57f737
Andres Carvajal <jc1993box@hotmail.com>
2020-05-13 02:47:23 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,22 @@
 class Author 
-  attr_accessor :name 
+  attr_accessor :name, :posts
   
   def initialize(author_name)
     @name= author_name
+    @posts = []
   end
   
-  def 
+  def add_post(post)
+    post.author = self
+    @posts << post
+  end
+  
+  def add_post_by_title
+    
+  
+end 
+
+
+
+
 end 
